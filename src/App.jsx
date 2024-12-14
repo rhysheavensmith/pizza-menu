681a996a52b895b8cf52054e672fb9813af03d73
Rhys Heaven-Smith <rhysheavensmith@gmail.com>
2024-12-15 00:37:30 +1300

    set up the folder structure and set up the map over the data for the pizzas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import './index.css';
+import { pizzaData } from './utils/data';
+
 const App = () => {
-	return <div>Hello World</div>;
+	return (
+		<div>
+			{pizzaData.map((pizza) => {
+				return (
+					<div key={pizza.name}>
+						<h2>{pizza.name}</h2>
+						<p>{pizza.ingredients}</p>
+						<p>{pizza.price}</p>
+						<img src={`src/assets/${pizza.photoName}`} alt={pizza.name} />
+						{pizza.soldOut && <p>Sold Out</p>}
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 
 export default App;
